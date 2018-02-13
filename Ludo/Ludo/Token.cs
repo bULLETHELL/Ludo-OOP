095d19e16970e3d271aa4c40e9094956a2c28788
SirPartyDyret <polyswing7@hotmail.com>
2018-02-13 12:49:28 +0100

    Added Color enum

--- Ludo/Ludo/Token.cs
+++ Ludo/Ludo/Token.cs
@@ -6,15 +6,25 @@ using System.Threading.Tasks;
 
 namespace Ludo
 {
-    class Token : ITokenPosition
+    public enum Color
+    {
+        Red,
+        Yellow,
+        Blue,
+        Green
+    };
+
+    public class Token : ITokenPosition
     {
         private int _x;
         private int _y;
+        public Color _color;
 
-        public Token(int x, int y)
+        public Token(int x, int y, Color color)
         {
             _x = x;
             _y = y;
+            _color = color;
         }
 
         public int x

7842ff5003253057c64559cae5fa4c433a69cc60
FunkyXive <andreasnordenhof@gmail.com>
2018-02-14 12:23:26 +0100

    Initial board prototype

--- Ludo/Ludo/Board.cs
+++ Ludo/Ludo/Board.cs
@@ -10,13 +10,36 @@ namespace Ludo
 
     class Board
     {
-        private 
+        private List<Square> BoardList = new List<Square>();
+        private string finalString;
         public Board()
         {
-            for(int i = 1; i < 52; i++)
+            int[] GlobeSquares = new int[] {1, 9, 14, 22, 27, 35, 40, 48};
+            int[] StarSquares = new int[] {6, 12, 19, 25, 32, 38, 45, 51 };
+            
+            for (int i = 1; i <= 52; i++)
             {
-
+                if (GlobeSquares.Contains(i))
+                {
+                    BoardList.Add(new Square(SquareType.Globe, i, Color.White));
+                }
+                else if (StarSquares.Contains(i))
+                {
+                    BoardList.Add(new Square(SquareType.Star, i, Color.White));
+                }
+                else
+                {
+                    BoardList.Add(new Square(SquareType.Normal, i, Color.White));
+                }
             }
+        }
+        public string Info()
+        {
+            foreach(Square square in BoardList)
+            {
+                finalString += square.Info();
+            }
+            return finalString;
         }
     }
 }

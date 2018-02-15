7086deaf2b73b9e1b3b6e61689c29a4fc47b3b2e
FunkyXive <andreasnordenhof@gmail.com>
2018-02-15 14:02:13 +0100

    Presentation demo

--- Ludo/Ludo/Game.cs
+++ Ludo/Ludo/Game.cs
@@ -12,22 +12,42 @@ namespace Ludo
         public Game()
         {
             Console.WriteLine("Welcome to LudoTM by Morten and Andreas");
-            AmountOfPlayers();
+            MakePlayers(AmountOfPlayers());
         }
-        private void AmountOfPlayers()
+        private int AmountOfPlayers()
         {
-            int entry;
+            char entry;
             //Console.Clear();
             Console.WriteLine("Please enter the amount of players with one key press");
             entry = Console.ReadKey().KeyChar;
-            if(entry == 2  ||entry == 3 || entry == 4)
+            if(entry == '2'  ||entry == '3' || entry == '4')
             {
+                Console.Clear();
                 Console.WriteLine(string.Format("starting game with {0} players", entry));
+                return (int)entry;
             }
             else
             {
-                Console.WriteLine("dick");
+                Console.WriteLine("Faulty entry, try again");
             }
+            return 0;
+        }
+        private List<Player> MakePlayers(int amountOfPlayers)
+        {
+            string playerName;
+            Color playerColor;
+            List<Player> players = new List<Player>();
+            List<Token> tokens = new List<Token>();
+            for (int i = 0; i < amountOfPlayers; i++)
+            {
+                Console.WriteLine(string.Format("Please enter the name of player {0}", i+1));
+                playerName = Console.ReadLine();
+                Console.WriteLine(string.Format("Please enter the desired colour for player {0}", i+1));
+                playerColor = (Color)Enum.Parse(typeof(Color), Console.ReadLine());
+                tokens.Add(new Token(i + 1, 0, playerColor, TokenState.Home));
+                players.Add(new Player(playerColor, playerName, tokens));
+            }
+            return (players);
         }
         private void PlayerNames()
         {
